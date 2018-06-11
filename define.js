85300674bc91b40db2290ee5df858d9f973af315
akaeda-yoshiki <y.a.05081996@gmail.com>
2018-06-11 23:31:24 +0900

    定義や列挙体などの不変の変数の記載ファイルの追加、図形に関する関数用のファイルの追加（中身は空）、ラベルの最大サイズを実装（縦横）

---
+++ define.js
@@ -0,0 +1,16 @@
+var LAVEL_SIZE_MAX_Y = 30;//ラベルの縦幅の上限
+var MARKER_RANGE = 8;//拡大縮小可能マーカーの輪郭からの反応幅
+
+var FIGURE = {//図形の状態
+    NULL: 0,
+    MOVE: 1,
+    CHANGE: 2,
+};
+var TYPE = {//図形の型
+    RECT: 0
+};
+
+var COLOR16 = {
+    GREEN: "#00ff00",
+    RED:"#ff0000"
+};
