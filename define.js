cca83926935700f2a8634fd5c1f8776b71d63dde
DESKTOP-C87FJFI\赤枝佳樹 <y.a.05081996@gmail.com>
2018-06-28 18:01:12 +0900

    新規評価項目があればマスターテーブルに書き込み

--- define.js
+++ define.js
@@ -2,6 +2,7 @@ var LAVEL_SIZE_MAX_Y = 50;//ラベルの縦幅の上限
 var RECT_MARKER_RANGE = 8;//拡大縮小可能マーカーの輪郭からの反応幅
 var CIRCLE_MARKER_RANGE = 6;//拡大縮小可能マーカーの輪郭からの反応幅
 var TRIANGLE_POINT_RNGE = 8;
+var ASSESSENT_INPUT_TEXT_NUM = 4;
 
 var FIGURE = {//図形の状態
     NULL: 0,
